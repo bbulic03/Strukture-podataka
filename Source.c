de129fe6b2d46821f9884f8e7ced4e52e810ae19
DomagojBajsic <115631216+DomagojBajsic@users.noreply.github.com>
2022-10-20 17:55:25 +0200

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,43 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-#define _CRT_SECURE_NO_WARNINGS
 #define MAX_LINE (1024)
+#define MAX_NAME (256)
 #define MAX_FILE_NAME (256)
 
-typedef struct{
-	char ime[MAX_LINE];
-	char prezime[MAX_LINE];
+struct _student;
+typedef struct _student {
+	char ime[MAX_NAME];
+	char prezime[MAX_NAME];
 	double bodovi;
 }student;
 
+int countstudents(FILE*);
+
+int maxbrbod(FILE*);
+
 int main(void) {
 	FILE* fp = NULL;
-	int count = 0, i,maxbrbod=50;
-	char buffer[MAX_LINE] = { 0 };
+	student* ps = NULL;
+	int i = 0;
 	double relativni;
 	char filename[MAX_FILE_NAME] = { 0 };
 
 	printf("Insert filename: ");
 	scanf(" %s", filename);
-	student* ps = NULL;
 
 	fp = fopen(filename, "r");
+	
+	ps = (student*)calloc(countstudents(fp), sizeof(student));
+	if (ps == NULL) {
+		// riješi problem
+	}
+	rewind(fp);
+
+	for (i = 0; i < countstudents(fp); i++)
+	{
+		fscanf(fp, "%s %s %lf\n", ps[i].ime, ps[i].prezime, &ps[i].bodovi);
+		relativni = ps[i].bodovi / maxbrbod(fp) * 100;
+		printf("Ime: %s\t Prezime: %s\t Bodovi: %lf\t Relativni bodovi: %lf\n", ps[i].ime, ps[i].prezime, &ps[i].bodovi, relativni);
+	}
+
+	printf("Broj studenata u datoteci je %d", countstudents(fp));
+	fclose(fp);
+
+	return 0;
+}
+
+int countstudents(FILE* fp) {
+	int count = 0;
+	char buffer[MAX_LINE] = { 0 };
+
 	while (!feof(fp)) {
+		fgets(buffer, MAX_LINE, fp);
 		if (strcmp("\n", buffer) != 0) {
 			count++;
 		}
 	}
-	ps = (student*)calloc((count), sizeof(student));
-	rewind(fp);
-	for ( i= 0; i < count; i++)
-	{
-		fscanf(fp, "%s %s %lf\n", (*(ps + i)).ime, (*(ps + i)).prezime, &(*(ps + i).bodovi));
-		relativni = ps[i].bodovi / maxbrbod*100;
-		printf("Ime: %s\t Prezime: %s\t Bodovi: %lf\t Relativni bodovi: %lf\n", (*(ps + i)).ime, (*(ps + i)).prezime, &(*(ps + i).bodovi), relativni);
+	return count;
+}
+
+int maxbrbod(FILE* fp, student* ps, int count) {
+	int max = 0;
+	int i;
+	count = countstudents(fp);
+
+	for (i = 0; i < count; i++) {
+		if (&ps[i].bodovi > max) {
+			max = &ps[i].bodovi;
+		}
 	}
-	printf("Broj studenata u datoteci je %d", count);
-	fclose(fp);
-	return 0;
+	return max;
 }
